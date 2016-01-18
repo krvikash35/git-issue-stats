37b5c002020015d52f20ce1db9ab68e41e11e694
Vikash <krvikash35@gmail.com>
2016-01-19 03:11:13 +0530

    changed server host

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 app.use(express.static("node_modules"));
 app.use(express.static("app"));
 
-app.listen(8081, function () {
+app.listen(8081, '0.0.0.0', function () {
   console.log("server listening on port: 8081");
 })
